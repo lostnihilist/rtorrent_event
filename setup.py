3c2d9e28b50e29be45bc6d6b0b24d71f917afb3a
lostnihilist <lostnihilist@gmail.com>
2024-09-15 21:26:32 -0700

    fix? setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,15 @@ if sys.version_info < (3, 5):
     sys.exit("Requires Python 3.5+.")
 
 setup(name='rtorrent_event',
-      version='0.1',
+      version='0.2',
       description="act on rtorrent events as reflected in session directory",
       author='lostnihilist',
       author_email='lostnihilist@gmail.com',
-      install_requires=['inotify',],
+      install_requires=['inotify',
+                        'bencode @ https://github.com/fuzeman/bencode.py.git',
+                        'daemons @ https://github.com/kevinconway/daemons.git'],
       keywords='rtorrent',
       scripts=['bin/rtorrent_event', 'bin/rtorrent_watch'],
       packages=['rtorrent_event'],
-      extras_require={
-          'daemon':['daemons']
-      }
      )
 
